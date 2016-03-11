3304989209103b11100e6e5828c7e499a4ed007d
Thomas Scheiblauer <tom@sharkbay.at>
2016-03-11 10:17:34 +0100

    modify clonut to handle default middleware

--- src/clonut/core.cljs
+++ src/clonut/core.cljs
@@ -7,43 +7,37 @@
 (defn- mutate [action state]
   (or (action state) state))
 
-(defn clonut [init-state & {:keys [action-buffer-size]
-                             :or {action-buffer-size 100}}]
+(defn clonut [init-state & {:keys [action-buffer-size init-state middleware]
+                            :or {action-buffer-size 100
+                                 init-state {}
+                                 middleware identity}}]
   (let [state-channel (chan)
         action-channel (chan action-buffer-size)]
     (pipe (async/map mutate [action-channel state-channel]) state-channel)
     (put! state-channel init-state)
-    [state-channel action-channel]))
+    {:state-channel state-channel
+     :action-channel action-channel
+     :middleware middleware}))
 
-(defn shutdown! [[state-channel action-channel]]
+(defn shutdown! [{:keys [state-channel action-channel]}]
   (close! action-channel)
   (close! state-channel))
 
-(defn react! [[_ action-channel] action-fn]
-  (put! action-channel action-fn))
+(defn react! [{:keys [action-channel middleware]} action-fn]
+  (put! action-channel (middleware action-fn)))
 
-(defn wrap-middleware
-  ([middleware]
-   (wrap-middleware middleware react!))
-  ([middleware react-fn]
-   (fn [clonut action-fn]
-     (react-fn clonut
-               (fn [state]
-                 (middleware state action-fn))))))
-
-(defn create-post-middleware [post-fn]
-  (fn react-middleware [state action-fn]
+(defn post-middleware [post-fn]
+  (fn [action-fn]
     (when-let [new-state (action-fn state)]
       (post-fn new-state))))
 
-(defn add-post-middleware
-  ([post-fn]
-   (wrap-middleware (create-post-middleware post-fn)))
-  ([post-fn react-fn]
-   (wrap-middleware (create-post-middleware post-fn) react-fn)))
+(defn pre-middleware [pre-fn]
+  (fn [action-fn]
+    (when-let [new-state (pre-fn state)]
+      (action-fn new-state))))
 
 (defn action-fn [f & args]
   (apply swapped-partial f args))
 
-(defn action [react-fn f]
-  (fn [& args] (react-fn (apply action-fn f args))))
+(defn action [f]
+  (fn [& args] (react! (apply action-fn f args))))
